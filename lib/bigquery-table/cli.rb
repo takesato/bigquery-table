eb6e5470b24c1b93595ea3f174c2a9dce7ac3c3a
takehito sato <takehitosato@gmail.com>
2015-03-25 15:18:55 +0900

    implement init command

--- lib/bigquery-table/cli.rb
+++ lib/bigquery-table/cli.rb
@@ -1,8 +1,18 @@
 require 'thor'
+require 'yaml'
 
 module BigqueryTable
   class CLI < Thor
-    def init(path = './')
+    include Thor::Actions
+
+    def self.source_root
+      File.dirname(__FILE__)
+    end
+
+    desc 'init [DEST]', 'initialize bigquery-table config'
+    def init(dest = '.')
+      copy_file 'templates/config.yml', "#{dest}/config.yml"
+      empty_directory 'schema'
     end
 
     def update
